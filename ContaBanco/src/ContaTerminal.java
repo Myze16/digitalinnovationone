6abe4cb2d1b9d4d94958a2d42438fb4aa2897f14
Mizael Pardal <mizaellucaspardal@hotmail.com>
2025-03-07 18:54:56 -0300

    feat: primeiro passo

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    short numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+     public static void main(String[] args) {
+        System.out.println("Inicio do programa...");
+     }
+}

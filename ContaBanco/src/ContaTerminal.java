cbc79c83a721c2eba5dc04c577d33042821200ec
Mizael Pardal <mizaellucaspardal@hotmail.com>
2025-03-08 01:24:25 -0300

    feat: resultado final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,8 +4,10 @@ public class ContaTerminal {
 
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
+
         System.out.println("Insira o número da conta: ");
-        short numero = sc.nextShort();
+        short numero = Short.parseShort(sc.nextLine());
 
         System.out.println("Insira a agencia da conta: ");
         String agencia = sc.nextLine();
@@ -14,8 +16,12 @@ public class ContaTerminal {
         String nomeCliente = sc.nextLine();
 
         System.out.println("Insira seu saldo: ");
-        double saldo = sc.nextDouble();
+        double saldo = Double.parseDouble(sc.nextLine());
+        sc.close();
 
+        System.out.println(String.format(
+              "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
+              nomeCliente, agencia, numero, saldo));
         
      }
 }
